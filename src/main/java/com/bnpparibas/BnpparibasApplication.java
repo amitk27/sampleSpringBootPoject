e30146268c3ddcff24dce69a5ede096108deb287
amitk27 <30648226+amitk27@users.noreply.github.com>
2025-10-19 01:48:25 +0530

    Update BnpparibasApplication.java

--- src/main/java/com/bnpparibas/BnpparibasApplication.java
+++ src/main/java/com/bnpparibas/BnpparibasApplication.java
@@ -2,12 +2,18 @@ package com.bnpparibas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BnpparibasApplication {
+public class BnpparibasApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BnpparibasApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BnpparibasApplication.class, args);
 	}
-
 }
