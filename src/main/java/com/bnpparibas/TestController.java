a19fdb5c3b29742705d169459d5e8f5779084fd0
Dev2 <Dev2@gmail.com>
2025-10-22 19:54:31 +0530

    d

--- src/main/java/com/bnpparibas/TestController.java
+++ src/main/java/com/bnpparibas/TestController.java
@@ -4,9 +4,11 @@ package com.bnpparibas;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/getData")
